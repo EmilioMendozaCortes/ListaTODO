3d8569e94a1d3c3b6b106bf2520f4323fe57aa58
EmilioMendozaCortes <emiliomendocor@gmail.com>
2024-10-15 16:00:27 -0600

    ToDoList PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
+// Estas instrucciones van el los servidores reales
+const cacheName = 'todoapp-offline';
+const preCache = [
+    './',
+    './index.html',
+    './mystyle.css',
+    './myscript.js',
+    './manifest.json'
+]
+
 // El self al inicia hace referencia al alcance global
 self.addEventListener('install', e=> {
     console.log('El SW esta instalado!!'); 
+    e.waitUntil(
+        caches.open(cacheName).then(cache => cache.addAll(preCache))
+    );
 });
